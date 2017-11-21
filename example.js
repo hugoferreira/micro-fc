cc5c1664014536137c1254677d9204c7edc43ab1
Hugo Sereno Ferreira <bytter@gmail.com>
2017-11-21 00:54:45 +0000

    * Support border and border color; * prt() is now print() * Mouse events now honour the border; * Init() function is called to initialize the program (if exists)

--- example.js
+++ example.js
@@ -1,11 +1,15 @@
-var x = 64, y = 30, frame = 0
-var xdir = 1
-var ydir = 1
+let x = 64, y = 30, frame = 0
+let xdir = 1
+let ydir = 1
+
+function init() {
+    border(1)
+}
 
 function update() {
     frame += 1
 
-    prt("o", x, y, frame % 16)
+    print("o", x, y, frame % 16)
 
     if (x > 128 - fontWidth - 1) xdir = -1
     else if (x == 0) xdir = 1
