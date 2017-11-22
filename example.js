09166c57f7484dfd3df96f294388199a0893be12
Hugo Sereno Ferreira <bytter@gmail.com>
2017-11-22 03:14:56 +0000

    First example now uses sprites.

--- example.js
+++ example.js
@@ -1,31 +1,30 @@
-let x = 64, y = 30, frame = 0
+let x = 64, y = 30
 let xdir = 1
 let ydir = 1
 
 function init() {
     border(1)
     cls(1)
+
+    sset(0, 2, 3, 15)
+    sset(0, 3, 3, 15)
+    sset(0, 4, 3, 15)
+    sset(0, 3, 2, 15)
+    sset(0, 3, 4, 15)
 }
 
 function update() {
-    frame += 1
+    if (x > 128 - 13) { beep(400); xdir = -1 }
+    else if (x == 0)  { beep(400); xdir = 1  }
 
-    if (x > 128 - fontWidth - 1) xdir = -1
-    else if (x == 0) xdir = 1
-
-    if (y > 128 - fontHeight) ydir = -1
-    else if (y == 0) ydir = 1
+    if (y > 128 - 13) { beep(440); ydir = -1 }
+    else if (y == 0)  { beep(440); ydir = 1 }
 
     x += xdir
     y += ydir
-
-    if (mouse.click) {
-        pset(mouse.x, mouse.y, 15)
-        beep(400)
-    }
 }
 
 function draw() {
     rectfill(20, 20, 80, 80, 12)
-    print("o", x, y, frame % 16)
+    spr(0, x, y)
 }
