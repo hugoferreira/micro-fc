7f95e95e4a7c844e44a43b63ca604d26aa86cb41
Hugo Sereno Ferreira <bytter@gmail.com>
2017-11-23 16:53:13 +0000

    Dummy commit

--- sound.js
+++ sound.js
@@ -1,17 +1,16 @@
-var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-var frequencyOffset = 0;
-var oscillator = audioContext.createOscillator();
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const frequencyOffset = 0;
+const oscillator = audioContext.createOscillator();
 oscillator.type = 'triangle';
-var gainNode = audioContext.createGain();
+const gainNode = audioContext.createGain();
 oscillator.connect(gainNode);
 gainNode.connect(audioContext.destination);
 gainNode.gain.value = 0;
 oscillator.frequency.value = 400;
 oscillator.start(0);
-function beep(freq) {
-    if (freq === void 0) { freq = 400; }
+function beep(freq = 400) {
     oscillator.frequency.value = freq;
     gainNode.gain.value = 0.5;
-    setTimeout(function () { gainNode.gain.value = 0; }, 100);
+    setTimeout(() => { gainNode.gain.value = 0; }, 100);
 }
 //# sourceMappingURL=sound.js.map
